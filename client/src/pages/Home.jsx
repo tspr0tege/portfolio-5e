4de84fa93e6e3b3f751722d3dcca36b523ab83ec
Squall Leonhart <squallpl1983@gmail.com>
2022-09-15 12:12:41 -0400

    Homepage first draft completed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -86,7 +86,11 @@ export default () => {
             <Card.Title>Skills Summary</Card.Title>
           </Card.Header>
           <Card.Body>
-            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit rem dolore nulla, ipsum voluptates repudiandae deleniti quisquam minima exercitationem, dignissimos mollitia sequi nemo harum, quam quos quo quia quasi reprehenderit? </p>
+            <p> I've spent my time learning the core skills I gained from my coding education, well enough to teach. My efforts being guided by two core philosophies: </p>
+            <p>Gaining a better understanding over fewer skills, as taught by Bruce Lee.</p>
+            <p>Learning with the intent to teach, requires greater understanding and ownership of the material.</p>
+            <p>I'm happy to say my efforts have paid off for both myself, and the people I've worked with. And to sum up my skills I would say:</p>
+            <q>If you can find it on the internet, I can build it.</q>
           </Card.Body>
         </Card>
       </div>
@@ -220,7 +224,15 @@ export default () => {
             <Card.Title>About</Card.Title>
           </Card.Header>
           <Card.Body>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam magni sint laudantium quod voluptate quae. Sunt provident illo atque deleniti eaque odit modi obcaecati? Ut est fuga maiores optio earum?</p>
+            <p>I'm here to change the course of my destiny.</p>
+            
+            <p>As a youngling, video games inspired a curiosity about technology that led me to explore programming, networking, and 3D design in High School. After that, circumstances forced me into the job market and I did whatever was immediately available for a long time. Then at nearly 40, with a child getting close to High School I decided I needed to do whatever it took to create a successful path for him and myself.</p>
+            
+            <p>Once again, my love for video games drove me toward programming, as it blends my passion for complex puzzle solving, creativity, and contributing something that can have exponential impact on society. I angled towards web/software development specifically, as a practical matter.</p>
+
+            <p>So I stop dabbling and dedicated myself to learning HTML, CSS, and Javascript, but started to feel like I was spinning my tires and wasn't sure where to go from there. So I enrolled in Hack Reactor. After which I spent my time practicing my "one kick" as Bruce Lee taught, to hone my skills. At the same time I was teaching what I was learning, and have helped several University and boot camp students through their programs as a result. Also taking on projects where I could, while simultaneously dedicating myself to the job search.</p>
+
+            <p>At current, I've been involved with a really impressive machine learning project. Where I was originally brought on full-time, but due to funding issues had to be laid-off. I have, however, agreed to say on (on an equity basis) as the team and the experience have been phenomenal. I've genuinely enjoyed the process of conceptualizing how all the pieces work together and improving/adding to the codebase.</p>
           </Card.Body>
         </Card>
         <Card>
